1e9ac44c297575ba8f7e884fe6cf95949ecabfd4
sunyounghwang <sunyounghwang13@gmail.com>
2018-07-05 21:26:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 
-function destructivelyDeleteFromObjectByKey(object )
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
