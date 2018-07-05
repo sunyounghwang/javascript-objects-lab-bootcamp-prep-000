051c183b487cdce203ba8318d8558180415286ac
sunyounghwang <sunyounghwang13@gmail.com>
2018-07-05 21:20:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign(object, key, value);
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+  return obj;
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
